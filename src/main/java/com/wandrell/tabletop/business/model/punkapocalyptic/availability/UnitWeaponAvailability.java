f9b81fcbc811a7cc36dd5e1d5b32c06cfaddb616
Bernardo Martínez Garrido <programming@wandrell.com>
2014-10-23 11:04:49 +0200

    Added a class to store unit weapon availabilities.

---
+++ src/main/java/com/wandrell/tabletop/business/model/punkapocalyptic/availability/UnitWeaponAvailability.java
@@ -0,0 +1,30 @@
+package com.wandrell.tabletop.business.model.punkapocalyptic.availability;
+
+import java.util.Collection;
+
+import com.wandrell.tabletop.business.model.punkapocalyptic.inventory.Weapon;
+
+public interface UnitWeaponAvailability {
+
+    /**
+     * Returns the maximum amount of weapons the unit can acquire.
+     * 
+     * @return the maximum amount of weapons the unit can acquire
+     */
+    public Integer getMaxWeapons();
+
+    /**
+     * Returns the minimum amount of weapons the unit should acquire.
+     * 
+     * @return the minimum amount of weapons the unit should acquire
+     */
+    public Integer getMinWeapons();
+
+    /**
+     * Returns the weapon options.
+     * 
+     * @return the weapon options
+     */
+    public Collection<Weapon> getWeaponOptions();
+
+}
